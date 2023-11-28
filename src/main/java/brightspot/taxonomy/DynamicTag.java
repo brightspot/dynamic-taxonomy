caf75abd2b379e34e6c88b8a16057ce4d0aa3050
ninadkhalate <ninadkhalate93@gmail.com>
2023-11-28 17:29:36 -0500

    Remove hierarchy from DynamicTag

--- src/main/java/brightspot/taxonomy/DynamicTag.java
+++ src/main/java/brightspot/taxonomy/DynamicTag.java
@@ -1,24 +1,18 @@
 package brightspot.taxonomy;
 
-import brightspot.tag.Tag;
-import brightspot.taxon.TaxonParentExtension;
 import com.psddev.cms.db.Content;
-import com.psddev.cms.db.Taxon;
 import com.psddev.cms.db.ToolUi;
-import com.psddev.dari.db.Query;
 import com.psddev.dari.db.Record;
-import com.psddev.dari.db.StringException;
 import com.psddev.dari.util.DariUtils;
-import com.psddev.dari.util.ObjectUtils;
 
 import static brightspot.taxonomy.Utils.*;
 
 /**
- * Concrete implementation of {@link Tag} for dynamic taxonomies.
+ * Dynamic tag record for dynamic taxonomies.
  */
 @Content.Searchable
 @ToolUi.ExcludeFromGlobalSearch
-public class DynamicTag extends Record implements Tag, TaxonParentExtension {
+public class DynamicTag extends Record {
 
     @Required
     private String displayName;
@@ -28,11 +22,6 @@ public class DynamicTag extends Record implements Tag, TaxonParentExtension {
     @InternalName(DYNAMIC_TAXONOMY_NAME_FIELD)
     @ToolUi.ReadOnly
     private String name;
-
-    @Indexed
-    @InternalName(DYNAMIC_TAXONOMY_PARENT_FIELD)
-    @Where("_id != ?")
-    private DynamicTag parent;
 
     @Indexed
     @Required
@@ -57,14 +46,6 @@ public class DynamicTag extends Record implements Tag, TaxonParentExtension {
         this.name = name;
     }
 
-    public DynamicTag getParent() {
-        return parent;
-    }
-
-    public void setParent(DynamicTag parent) {
-        this.parent = parent;
-    }
-
     public DynamicTagCategory getCategory() {
         return category;
     }
@@ -76,55 +57,8 @@ public class DynamicTag extends Record implements Tag, TaxonParentExtension {
     // Overrides
 
     @Override
-    public String getTagDisplayNameRichText() {
-        return ObjectUtils.firstNonBlank(getDisplayName(), getName());
-    }
-
-    @Override
-    public String getTagDisplayNamePlainText() {
-        return ObjectUtils.firstNonBlank(getDisplayName(), getName());
-    }
-
-    @Override
-    public boolean isHiddenTag() {
-        // all tags are visible by default
-        return false;
-    }
-
-    @Override
-    public DynamicTag getTagParent() {
-        return getParent();
-    }
-
-    @Override
-    public String getLinkableText() {
-        return DariUtils.toNormalized(getName());
-    }
-
-    @Override
-    public TaxonParentExtension getTaxonParent() {
-        return getParent();
-    }
-
-    @Override
-    public Query<? extends Taxon> getTaxonChildrenQuery() {
-        return Query.from(DynamicTag.class)
-            .where(DYNAMIC_TAXONOMY_PARENT_INDEX + " = ?", this);
-    }
-
-    @Override
     protected void beforeCommit() {
         // normalize and set the internal name
         setName(DariUtils.toNormalized(getDisplayName()));
-    }
-
-    @Override
-    protected void onValidate() {
-        // validate that the category matches the parent category
-        if (!ObjectUtils.isBlank(getParent()) && (!ObjectUtils.equals(getParent().getCategory(), getCategory()))) {
-            getState().addError(
-                getState().getField(DYNAMIC_TAXONOMY_CATEGORY_FIELD),
-                new StringException("Tag category must match parent category."));
-        }
     }
 }
--- src/main/java/brightspot/taxonomy/HasDynamicTagsData.java
+++ src/main/java/brightspot/taxonomy/HasDynamicTagsData.java
@@ -2,17 +2,12 @@ package brightspot.taxonomy;
 
 import java.util.Collections;
 import java.util.HashSet;
-import java.util.LinkedHashSet;
-import java.util.LinkedList;
 import java.util.Optional;
-import java.util.Queue;
 import java.util.Set;
-import java.util.stream.Collectors;
 
 import com.psddev.cms.db.ToolUi;
 import com.psddev.dari.db.Modification;
 import com.psddev.dari.db.Recordable;
-import com.psddev.dari.util.UnresolvedState;
 
 import static brightspot.taxonomy.Utils.*;
 
@@ -41,40 +36,8 @@ public class HasDynamicTagsData extends Modification<HasDynamicTags> {
     @ToolUi.Hidden
     @ToolUi.Filterable(displayByDefault = true)
     @DisplayName("Tags")
-    public Set<DynamicTag> getTagsAndAncestors() {
-        Set<DynamicTag> tagsAndAncestors = new LinkedHashSet<>();
-
-        Set<DynamicTag> tags = UnresolvedState.resolveAndGet(getOriginalObject(), HasDynamicTags::getDynamicTags);
-
-        if (tags == null) {
-            return Collections.emptySet();
-        }
-
-        Set<DynamicTag> visited = new HashSet<>();
-        Queue<DynamicTag> toProcess = new LinkedList<>(tags);
-
-        while (!toProcess.isEmpty()) {
-            DynamicTag next = toProcess.remove();
-            if (!visited.add(next)) {
-                continue;
-            }
-
-            tagsAndAncestors.add(next);
-
-            Optional.ofNullable(UnresolvedState.resolveAndGet(next, DynamicTag::getTagParent))
-                .ifPresent(toProcess::add);
-        }
-
-        return tagsAndAncestors;
-    }
-
-    @Indexed
-    @ToolUi.Hidden
-    public Set<DynamicTag> getVisibleTagsAndAncestors() {
-
-        return getTagsAndAncestors()
-            .stream()
-            .filter(tag -> !tag.isHiddenTag())
-            .collect(Collectors.toSet());
+    public Set<DynamicTag> getTags() {
+        return Optional.ofNullable(getOriginalObject())
+            .map(HasDynamicTags::getDynamicTags).orElse(Collections.emptySet());
     }
 }
